22198ea05e08f99a58bc68b276cf688f8df09086
wayne-otido <otidowayne@gmail.com>
2023-09-22 15:14:26 +0300

    done

--- server/app.py
+++ server/app.py
@@ -46,6 +46,38 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        plant.is_in_stock = False
+
+        for attr in request.form:
+            setattr(plant, attr, request.form[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response(
+            
+        )
+
+        return response
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
